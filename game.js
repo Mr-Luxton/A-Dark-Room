39c729c084ca7a00589203e4ce1e3e557c8bd9c3
Mr-Luxton <malakisutton3@gmail.com>
2025-07-08 11:02:21 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,29 @@
-let canvas, ctx, player;
+let canvas, ctx, player, particles, collectibles, bgLayers = [];
+
+function loadBG() {
+  const urls = ['assets/bg_layer1.png', 'assets/bg_layer2.png', 'assets/bg_layer3.png'];
+  bgLayers = urls.map((src, i) => {
+    const img = new Image(); img.src = src;
+    return { img, x: 0, speed: (i + 1) * 0.2 };
+  });
+}
 
 function initGame() {
   canvas = document.getElementById("game-canvas");
   ctx = canvas.getContext("2d");
   resizeCanvas();
-
+  loadBG();
   player = new Player(canvas.width / 2, canvas.height - 100);
-
+  particles = new ParticleSystem(canvas);
+  collectibles = new CollectibleSystem(canvas);
   window.addEventListener("resize", resizeCanvas);
+  animateFade();
+}
+
+function animateFade() {
+  const fade = document.getElementById("fade-overlay");
+  fade.classList.remove("fade-out");
+  setTimeout(() => fade.classList.add("fade-out"), 100);
   requestAnimationFrame(gameLoop);
 }
 
@@ -19,33 +35,29 @@ function resizeCanvas() {
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBackground();
-  player.update();
-  player.draw(ctx);
+  particles.update(); particles.draw(ctx);
+  collectibles.update(player); collectibles.draw(ctx);
+  player.update(); player.draw(ctx);
   requestAnimationFrame(gameLoop);
 }
 
 function drawBackground() {
-  let grd = ctx.createRadialGradient(
-    canvas.width / 2, canvas.height / 2, 100,
-    canvas.width / 2, canvas.height / 2, canvas.width / 1.5
-  );
-  grd.addColorStop(0, "#111");
-  grd.addColorStop(1, "#000");
-
-  ctx.fillStyle = grd;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = "#111";
+  bgLayers.forEach((layer, i) => {
+    layer.x = (layer.x - layer.speed) % canvas.width;
+    ctx.globalAlpha = 1 - i * 0.3;
+    ctx.drawImage(layer.img, layer.x, 0, canvas.width, canvas.height);
+    ctx.drawImage(layer.img, layer.x + canvas.width, 0, canvas.width, canvas.height);
+  });
+  ctx.globalAlpha = 1;
+  ctx.fillStyle = '#111';
   ctx.beginPath();
   ctx.moveTo(0, canvas.height - 60);
   ctx.quadraticCurveTo(canvas.width / 2, canvas.height - 100, canvas.width, canvas.height - 60);
   ctx.lineTo(canvas.width, canvas.height);
   ctx.lineTo(0, canvas.height);
-  ctx.closePath();
-  ctx.fill();
+  ctx.closePath(); ctx.fill();
 }
 
 function startGame() {
   initGame();
 }
-
